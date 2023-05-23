be148b3d214fc7c9095a8604e39626465004b39b
loizage <zagemmm@icloud.com>
2023-05-23 15:49:10 +0100

    movement and spawn on client side

--- packages/client/src/mud/createSystemCalls.ts
+++ packages/client/src/mud/createSystemCalls.ts
@@ -8,21 +8,38 @@ export type SystemCalls = ReturnType<typeof createSystemCalls>;
 
 export function createSystemCalls(
   { singletonEntity, playerEntity, worldSend, txReduced$ }: SetupNetworkResult,
-  components: ClientComponents
+  { Player, Position }: ClientComponents
 ) {
   const moveTo = async (x: number, y: number) => {
-    // TODO
-    return null as any;
+    if (!playerEntity) {
+      throw new Error("no player");
+    }
+    const tx = await worldSend("move",[x,y]);
+    await awaitStreamValue(txReduced$, (txHash) => txHash === tx.hash);
   };
 
   const moveBy = async (deltaX: number, deltaY: number) => {
-    // TODO
-    return null as any;
+    if (!playerEntity) {
+      throw new Error("no player");
+    }
+    const playerPosition = getComponentValue(Position, playerEntity);
+    if (!playerPosition) {
+      console.warn("cannot moveBy without player position, not yet spawned?");
+      return;
+    }
+    await moveTo(playerPosition.x + deltaX, playerPosition.y + deltaY);
   };
 
   const spawn = async (x: number, y: number) => {
-    // TODO
-    return null as any;
+    if (!playerEntity) {
+      throw new Error("no player");
+    }
+    const canSpawn = getComponentValue(Player, playerEntity)?.value !== true;
+    if (!canSpanw) {
+      throw new Error("already spawned");
+    }
+    const tx = await worldSend("spawn", [x,y]);
+    await awaitStreamValue(txReduced$, (txHash) => txHash === tx.hash);
   };
 
   const throwBall = async () => {
